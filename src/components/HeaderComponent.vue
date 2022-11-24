a9dd38614c1b817228a05633f1757a568f03bfa9
Maiara santo <maiarasantos34@yahoo.com>
2022-11-24 20:43:15 -0300

    config04

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="w-screen h-[4.5rem]  background-color: #27272A backdrop-blur-sm shadow-sm fixed flex justify-center items-center">
-        <div class="px-5 py-2 box-border  bg-color: rgba(218, 218, 218, 0.33) rounded-xl hover:brightness-90 cursor-pointer transition-all">
+    <header class="w-screen h-[4.5rem] bg-glass backdrop-blur-sm shadow-sm fixed flex justify-center items-center">
+        <div class="px-5 py-2 box-border bg-dark-500 rounded-xl hover:brightness-90 cursor-pointer transition-all">
             <img
                 class="w-36 h-7"
                 src="@/assets/icons/stackx.svg"
