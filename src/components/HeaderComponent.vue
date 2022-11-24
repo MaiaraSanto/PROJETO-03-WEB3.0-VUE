8781c7e1d8bec6237f6a98858296bcc9d45e38d0
Maiara santo <maiarasantos34@yahoo.com>
2022-11-24 18:52:26 -0300

    init repositorio

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="w-screen h-[4.5rem] bg-glass backdrop-blur-sm shadow-sm fixed flex justify-center items-center">
+        <div class="px-5 py-2 box-border bg-dark-500 rounded-xl hover:brightness-90 cursor-pointer transition-all">
+            <img
+                class="w-36 h-7"
+                src="@/assets/icons/stackx.svg"
+                alt="">
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
---
+++ src/components/TechsComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="w-full h-40 bg-dark-500 rounded-2xl">
+        <ul class="list-disc text-white-400 h-full flex flex-col justify-center items-center flex-wrap">
+          <li 
+            class="mb-1 w-[10rem]"
+            v-for="tech in techs" :key="tech"
+          >
+            {{tech}}
+          </li>
+        </ul>
+      </section>
+</template>
+
+<script>
+  export default {
+    name: 'TechsComponent',
+    data() {
+      return {
+        techs: [
+            'HTML', 
+            'SASS', 
+            'JavaScript', 
+            'TypeScript', 
+            'React', 
+            'Styled-Components', 
+            'Java', 
+            'Spring', 
+            'VueJs', 
+            'Tailwind', 
+            'Bootstrap', 
+            'Angular', 
+            'MYSql',
+            'PostgreSQL',
+            'PL/SQL',
+            'MariaDB',
+            'Firebird',
+            'SQLServer'
+          ]
+      }
+    }
+  }
+</script>
