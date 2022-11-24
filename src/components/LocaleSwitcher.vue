17a362671b2f017bde2c6744d7b5e6d412c580c5
Maiara santo <maiarasantos34@yahoo.com>
2022-11-24 19:12:09 -0300

    config2

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -2,7 +2,17 @@
     <nav class="mx-auto flex">
         <button 
           @click="$i18n.locale = locales[0]"
-          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12  background-color: #27272A flex items-center justify-center rounded-2xl text-white-900"
+          class="height: 4.1rem
+    background-color: #27272A
+    border-radius: 0.8rem
+    border: none
+    padding: 1rem 1.9rem
+    margin-left: 3rem
+    display: flex
+    justify-content: space-around
+    align-items: center
+    gap: 0.5rem
+    border: 1px solid #9d4b00"
         >
             <img class="mr-1" src="@/assets/icons/brazil.svg" alt=""/>
             Português
@@ -10,7 +20,17 @@
 
         <button 
           @click="$i18n.locale = locales[1]"
-          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12  background-color: #27272A flex items-center justify-center rounded-2xl text-white-900"
+          class="height: 4.1rem
+    background-color: #27272A
+    border-radius: 0.8rem
+    border: none
+    padding: 1rem 1.9rem
+    margin-left: 3rem
+    display: flex
+    justify-content: space-around
+    align-items: center
+    gap: 0.5rem
+    border: 1px solid #9d4b00"
         >
             <img class="mr-1" src="@/assets/icons/english.svg" alt=""/>
             Inglês
@@ -18,7 +38,17 @@
 
         <button 
           @click="$i18n.locale = locales[2]"
-          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 bg-dark-500 flex items-center justify-center rounded-2xl text-white-900"
+          class="height: 4.1rem
+    background-color: #27272A
+    border-radius: 0.8rem
+    border: none
+    padding: 1rem 1.9rem
+    margin-left: 3rem
+    display: flex
+    justify-content: space-around
+    align-items: center
+    gap: 0.5rem
+    border: 1px solid #9d4b00"
         >
             <img class="mr-1" src="@/assets/icons/spanish.svg" alt=""/>
             Espanhol
