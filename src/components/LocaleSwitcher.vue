b5a5271f873f43e4841fc5f4aebe71c80fb4fce3
Maiara santo <maiarasantos34@yahoo.com>
2022-11-24 19:25:06 -0300

    config2

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -2,17 +2,7 @@
     <nav class="mx-auto flex">
         <button 
           @click="$i18n.locale = locales[0]"
-          class="height: 4.1rem
-    background-color: #27272A
-    border-radius: 0.8rem
-    border: none
-    padding: 1rem 1.9rem
-    margin-left: 3rem
-    display: flex
-    justify-content: space-around
-    align-items: center
-    gap: 0.5rem
-    border: 1px solid #9d4b00"
+          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 background-color: #27272A flex items-center justify-center rounded-2xl text-white-900  border: 1px solid #9d4b00  gap: 0.5rem  align-items: center  justify-content: space-around  margin-left: 3rem  padding: 1rem 1.9rem border: none border-radius: 0.8rem  height: 4.1rem "
         >
             <img class="mr-1" src="@/assets/icons/brazil.svg" alt=""/>
             Português
@@ -20,17 +10,7 @@
 
         <button 
           @click="$i18n.locale = locales[1]"
-          class="height: 4.1rem
-    background-color: #27272A
-    border-radius: 0.8rem
-    border: none
-    padding: 1rem 1.9rem
-    margin-left: 3rem
-    display: flex
-    justify-content: space-around
-    align-items: center
-    gap: 0.5rem
-    border: 1px solid #9d4b00"
+          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 background-color: #27272A flex items-center justify-center rounded-2xl text-white-900"
         >
             <img class="mr-1" src="@/assets/icons/english.svg" alt=""/>
             Inglês
@@ -38,17 +18,7 @@
 
         <button 
           @click="$i18n.locale = locales[2]"
-          class="height: 4.1rem
-    background-color: #27272A
-    border-radius: 0.8rem
-    border: none
-    padding: 1rem 1.9rem
-    margin-left: 3rem
-    display: flex
-    justify-content: space-around
-    align-items: center
-    gap: 0.5rem
-    border: 1px solid #9d4b00"
+          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 background-color: #27272A flex items-center justify-center rounded-2xl text-white-900"
         >
             <img class="mr-1" src="@/assets/icons/spanish.svg" alt=""/>
             Espanhol
