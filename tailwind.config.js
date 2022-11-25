c6364cf4264ea922a1f4f20221af0656998da078
Maiara santo <maiarasantos34@yahoo.com>
2022-11-25 02:06:15 -0300

    dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,6 @@ module.exports = {
       'grey-300': '#C6C6D3',
       'grey-400': '#A1A1AA',
       
-      'dark-300': '#179C17',
-      'dark-400': '#116F11',
-      'dark-500': '#0F4F0F',
-      'dark-600': '#0F360F',
-      'dark-700': '#091909',
 
       'glass': 'rgba(15, 79, 15, 0.3)'
       
